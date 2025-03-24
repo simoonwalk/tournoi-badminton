8aa03ccb3beeed40c87dce2ee21e9227fad88eb7
simoonwalk <32705724+simoonwalk@users.noreply.github.com>
2025-03-24 16:12:39 +0100

    Update supabase_client.py

--- supabase_client.py
+++ supabase_client.py
@@ -1,1 +1,8 @@
+ from supabase import create_client
 
+# Remplace par tes infos personnelles Supabase
+SUPABASE_URL = "https://oaacsxbwcdnpncxswxsx.supabase.co"
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9hYWNzeGJ3Y2RucG5jeHN3eHN4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI4Mjc5NDgsImV4cCI6MjA1ODQwMzk0OH0.pLVwF75onyhNeDtlCvvKH7OSh8AFiT4cE5XNGOvqOdE"  # ta clé anon publique
+
+def init_supabase():
+    return create_client(SUPABASE_URL, SUPABASE_KEY)
